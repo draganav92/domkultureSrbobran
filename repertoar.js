5e046a7348fc0196e19636f9c4ef5061dbfb6f54
draganav92 <118554343+draganav92@users.noreply.github.com>
2024-04-17 18:36:14 +0200

    Add files via upload

---
+++ repertoar.js
@@ -0,0 +1,386 @@
+var predstave = [
+    {
+        naziv: "Ко је убио Џенис Џоплин?",
+        datum: "28.04.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Соња Петровић",
+        trajanje: "2 сата",
+        slika: "slike/dzenisdzoplin.jpg",
+        zanr: "драма", 
+        uloge: "Бојана Милановић, Соња Кеслер, Стефан Вукић, Димитрије Аранђеловић, Вукашин Ранђеловић, Петар Бањац, Филип Грубач, Игор Сакач",
+        opis: "Данас нам је тешко да замислимо време у којем је женама било забрањено да носе панталоне или у којем је било недопустиво да се баве рокенролом, али Џенис Џоплин је одрастала управо у једном таквом времену. "
+        + "Нажалост, Џенис Џоплин данас ређе памтимо по томе шта је све урадила за женску борбу и еманципацију жена, а чешће по начину на који је живела, па на крају и умрла, а који махом осуђујемо. "
+    },
+    {
+        naziv: "Била једном једна земља",
+        datum: "29.04.2024",
+        termin: "19:00",
+        cena: 1100,
+        reziser: "Кокан Младеновић",
+        trajanje: "2 сата",
+        slika: "slike/bilajednomjednazemlja.jpg",
+        zanr: "драма",
+        uloge: "Марко Марковић, Марта Береш, Петар Попара Црни: Милован Филиповић, Радоје Чупић, Аљоша Ђидић, "
+        + "Југослав Крајнов, Бојана Милановић, Гордана Ђурђевић Димић, Пеђа Марјановић, Анђела Пећинар, "
+        + "Душан Јакишић, Ненад Пећинар, Александар Гајин, Александра Плескоњић, Миа Симоновић, Бранислав Јерковић, "
+        + "Тијана Максимовић, Јелена Антонијевић, Ивана Панчић Добродолац",
+        opis: " „Била једном једна…“, тако обично почињу бајке. Бајкама се деца успављују, "
+        + "али и плаше. Ово је, у неку руку, жанровски драматуршко-редитељски кључ, помоћу којега смо, у представи "
+        + "Била једном једна земља, настојали да представимо драмски свет Душана Ковачевића. "
+        + "При том представљању, водило нас је чврсто уверење да не само ликови и збивања истоименог романа, "
+        + "као и његове драмске „претече“ Пролеће у јануару, него и мотиви и значења Ковачевићевих магистралних "
+        + "комада и сценарија "
+    },
+    {
+        naziv: "Kaко сам постала фрајла",
+        datum: "30.04.2024",
+        termin: "20:00",
+        cena: 1200,
+        reziser: "Вишња Обрадовић",
+        trajanje: "2 сата",
+        slika: "slike/kakosampostalafrajla.jpg",
+        zanr: "комедија", 
+        uloge: " Višnja Obradović, Isidora Vlček, Slavica Ljujić",
+        opis: "Три „жене на све спремне” су уз музику, љубав и пријатељство као покретачке снаге показале "
+        + "на један несвакидашњи, духовит начин, препун смеха живот иза кулиса и живот на точковима, "
+        + "као и изазове шоубизниса."
+    },
+    {
+        naziv: "Враголанка",
+        datum: "03.05.2024",
+        termin: "20:30",
+        cena: 800,
+        reziser: "Калин Ханциу",
+        trajanje: "1.5 сати",
+        slika: "slike/vragolanka.jpg",
+        zanr: "балет", 
+        uloge: "Рајна Ремовић, Лана Стојановић, Самјуел Бишоп, Давид Груосо, Милан Иван, Андреј Колчериу, "
+        + "Давид Груосо, Самјуел Бишоп, Александар Бечварди, Мина Радовић, Зоран Трифуновић, Наојуки Ацуми",
+        opis: "Ово дело носи најпозитивнију балетску музику у целокупној литератури, из ње се чита оптимизам "
+        + "на који нико не остаје равнодушан па ни наша публика, која је ову обнову дефинитвно чекала са великим "
+        + "нестрпљењем. Играчи су назаустављиви на сцени, понети том атмосфером, и музичари уживају у извођењу "
+        + "разигране „Враголанке“. "
+    },
+    {
+        naziv: "Ујеж",
+        datum: "03.05.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Радослав Миленковић",
+        trajanje: "2 сата",
+        slika: "slike/ujez.jpg",
+        zanr: "комедија", 
+        uloge: "Радослав Миленковић, Гордана Јошић, Јована Мишковић, Аљоша Ђидић, Марија Меденица, "
+        + "Александар Гајин, Мирослав Фабри, Марко Савковић, Александра И. Плескоњић, Сања Микитишин "
+        + "Лидија Стевановић, Бојана Милановић Поповић, Јована Стипић Радошевић, Јелена Антонијевић, "
+        + "Горица Регодић, Љубица Ракић, Пеђа Марјановић, Сања Ристић Крајнов, Димитрије Аранђеловић", 
+        opis: " Сваку узвишену идеју увек прати и њено изопачење. Комедија се увек бави тим изопачењем. "
+        + "У земљи Србији, где је тако нагао и вртоглав скок из обојака у смокинг и тако кратка дистанца "
+        +"од чучавца до јавне говорнице на коју се пењу такозвани јавни радници, комедија не може избећи "
+        + "трагичну страну смешног као ни комичну страну трагичног. "
+    },
+    {
+        naziv: "Вештице из Салема",
+        datum: "05.05.2024",
+        termin: "20:00",
+        cena: 1100,
+        reziser: "Никита Миливојевић",
+        trajanje: "2 сата",
+        slika: "slike/vesticeizsalema.jpg",
+        zanr: "драма", 
+        uloge:"Ивана Панчић Добродолац, Теодора Марчета, Миа Симоновић, Николина Вујевић, Нина Рукавина "
+        + "Соња Кеслер, Алиса Лацко, Бранислав Јерковић, Марија Меденица, Душан Вукашиновић, Мирослав Фабри "
+        + "Драгиња Вогањац, Мирјана Гардиновачки, Душан Јакишић, Лидија Стевановић, Александар Гајин",
+        opis: "У драми Артура Милера само је привидно реч о „историјским вештицама“, писац је, као што знамо, "
+        + "мислио на „лов на вештице“ који је захватио САД 50-их година прошлог века, када је сенатор "
+        + "Макарти прогонио комунисте и све који су с њима имали везе. "
+    },
+    {
+        naziv: "Не мо`ш побећи од недеље",
+        datum: "09.05.2024",
+        termin: "20:00",
+        cena: 900,
+        reziser: "Тамара Кострешевић",
+        trajanje: "2 сата",
+        slika: "slike/nemospobeciodnedelje.jpg",
+        zanr: "драма",
+        uloge: "Анђела Пећинар, Аљоша Ђидић", 
+        opis: "„Немреш побјећ од недјеље“ је дуо-драма Тене Штивичић, о односима између партнера. "
+        + "То је ненаметљива комедија о љубави, заједничком животу и проблемима који пре или касније "
+        + "искрсну у већини веза или бракова. "
+    },
+    {
+        naziv: "Женидба на чардаку",
+        datum: "10.05.2024",
+        termin: "19:30",
+        cena: 1000,
+        reziser: "Соња Петровић",
+        trajanje: "2 сата",
+        slika: "slike/zenidbanacardaku.jpg",
+        zanr: "драма", 
+        uloge: "Југослав Крајнов, Наташа Илин, Љубиша Милишић, Сања Ристић-Крајнов, Сања Микитишин, Миљан Давидовић",
+        opis: "Представа “Женидба на чардаку” по тексту Ј. С. Поповића,, Женидба и удадба”, а по драматизацији "
+        + "Петра Петровића настала је по угледу на представу “Женидба и удадба” Богољуба Браше Крајнова из 1978, "
+        + "у извођењу Омладинског позоришта у Жабљу која је као једна од најбољих аматерских представа ондашње "
+        + "СФРЈ(победила на тадашњем савезном фестивалу аматерских позоришта у Требињу `78. "
+    },
+    {
+        naziv: "Мишја грозница",
+        datum: "12.05.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Ђорђе Нешовић",
+        trajanje: "2 сата",
+        slika: "slike/misjagroznica.jpg",
+        zanr: "драма", 
+        uloge: "Сања Ристић Крајнов, Миа Симоновић, Марко Савић, Димитрије Аранђеловић, Вукашин Ранђеловић, Александар Сарапа",
+        opis: "У овој представи покушали смо да обрадимо тему склапања примирја, и целокупне његове спектакуларизације. "
+        + "Појам мира (примирја) на нашим просторима је нека врста утопије, егзотике, којој се сви надамо и "
+        + "непрестано очекујемо. " 
+    },
+    {
+        naziv: "Алабама",
+        datum: "15.05.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Рахим Бурхан",
+        trajanje: "2 сата",
+        slika: "slike/alabama.jpg",
+        zanr: "драма", 
+        uloge: "Бланка Барт, Горан Коши, Сњежана Ланчић, Моника Дувњак, Мијо Павелко, Горан Вучко, Драшко Зидар ", 
+        opis: "'Алабама' је драма о друштвеном слоју којем се, у помало патронизирајућим навалама симпатије, "
+        + "у колоквијалном говору често тепа називајући их се „малим људима“. Шпишићева драма јасно упућује "
+        + "на чињеницу да Горани Бурићи никад не настају сами од себе."
+    },
+    {
+        naziv: "Царство мрака",
+        datum: "16.05.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Игор Вук Торбица",
+        trajanje: "2 сата",
+        slika: "slike/carstvomraka.jpg",
+        zanr: "драма", 
+        uloge: "Олга Одановић, Никола Милановић, Уна Нинић, Милица Ивошевић, Анастасија Станић", 
+        opis: "Неколико година пре него што ће Лав Николајевич Толстој написати комад Царство мрака, "
+        + "негде у руској провинцији заиста је забележен случај човека који на дан свадбе своје ћерке "
+        + "јавно признаје све злочине које је починио и тражи искупљење грехова."
+    },
+    {
+        naziv: "Севиљски берберин",
+        datum: "21.05.2024",
+        termin: "20:00",
+        cena: 1000,
+        reziser: "Иван Клеменц",
+        trajanje: "2 сата",
+        slika: "slike/seviljskiberberin.jpg",
+        zanr: "опера", 
+        uloge: "Саша Штулић, Небојша Бабић, Јелена Кончар, Васа Стајкић, Горан Крнета, Андриана Петровски, "
+        + "Бранислав Станков, Владимир Стојковић, Стајић Драган ",
+        opis: "Севиљски берберин је комична опера у два чина (три слике), италијанског композитора Ђоакина Росинија. "
+        + "Либрето је написао Стербини, према истоименој комедији Бомаршеа. Премијерно је изведена 20. фебруара 1816. године, у Риму."
+    },
+    {
+        naziv: "Дејт",
+        datum: "30.05.2024",
+        termin: "20:00",
+        cena: 1400,
+        reziser: "Сандра Силађев",
+        trajanje: "2 сата",
+        slika: "slike/dejt.jpg",
+        zanr: "драма", 
+        uloge: "Сандра Силађев - монодрама", 
+        opis: "Оливера се осећа као дама, уседелица, бабадевојка и распуштеница, чак и као удовица мада "
+        + "нико није умро. Слуђена је категоризацијама и чудним односима између љубавника у 21. веку. "
+        + "Сматра да не тражи много од живота, само и једноставно оно што сваки одрасли појединац жели "
+        + "за себе – да пронађе своју сродну душу. Има ли је за Оливеру на овом целом свету? Или бар на интернету?"
+    },
+];
+
+
+
+// Funkcija za prikazivanje predstava
+function prikaziPredstave(pred) {
+    var container = document.getElementById("predstave-container");
+    container.innerHTML = ""; // Clear existing content
+
+    // Iteriramo kroz sve predstave
+    pred.forEach(function(predstava) {
+        var div = document.createElement("div");
+        console.log(predstava.naziv);
+        div.classList.add("predstava-div");
+        // Dodajemo HTML za prikaz informacija o predstavi
+        var slikaDiv = document.createElement("div");
+        slikaDiv.classList.add("slika-div");
+        var slika = document.createElement("img");
+        slika.src = predstava.slika;
+        slika.alt = "Slika predstave";
+        slikaDiv.appendChild(slika);
+        div.appendChild(slikaDiv);
+
+        var podaciDiv = document.createElement("div");
+        podaciDiv.classList.add("podaci-div");
+        podaciDiv.innerHTML = `
+            <h2>${predstava.naziv}</h2>
+            <p>Жанр: ${predstava.zanr}</p>
+            <p>Датум: ${predstava.datum}</p>
+            <p>Термин: ${predstava.termin}</p>
+            <p>Цена улазнице: ${predstava.cena} дин.</p>
+            <p>Режисер: ${predstava.reziser}</p>
+            <p>Tрајање: ${predstava.trajanje}</p>
+            <button onclick="popup('${predstava.naziv}')">Погледај детаље</button>
+            <button onclick="window.location.href = 'rezervacija.html'">Резервиши улазнице</button>`;
+        div.appendChild(podaciDiv);
+        
+        container.appendChild(div);
+        container.innerHTML += "<hr></hr>";
+    });
+}
+
+// Funkcija za pretragu predstava po nazivu
+function pretrazi() {
+    var input = document.getElementById("pretraga").value.toLowerCase();
+    var filtriranePredstave = predstave.filter(function(predstava) {
+        return predstava.naziv.toLowerCase().includes(input);
+    });
+    prikaziPredstave(filtriranePredstave);
+}
+
+
+// Funkcija za popunjavanje padajućeg menija sa žanrovima
+function popuniZanrove() {
+    var zanrovi = [];
+    // Iteriramo kroz sve predstave i dodajemo jedinstvene žanrove u niz
+    predstave.forEach(function(predstava) {
+        if (!zanrovi.includes(predstava.zanr)) {
+            zanrovi.push(predstava.zanr);
+        }
+    });
+    // Sortiramo niz žanrova po abecednom redu
+    zanrovi.sort();
+    // Popunjavamo padajući meni sa žanrovima
+    var zanrSelect = document.getElementById("zanr");
+    zanrovi.forEach(function(zanr) {
+        var option = document.createElement("option");
+        option.value = zanr.toLowerCase(); // Postavljamo vrednost malim slovima radi doslednosti
+        option.textContent = zanr;
+        zanrSelect.appendChild(option);
+    });
+    // Dodajemo event listener za promenu vrednosti padajućeg menija
+    zanrSelect.addEventListener('change', function() {
+        filtrirajPoZanru();
+    });
+}
+
+
+
+// Dodajemo event listener na padajući meni za žanrove
+document.getElementById("zanr").addEventListener('change', function() {
+    filtrirajPoZanru(); // Pozivamo funkciju za filtriranje kada se izabere novi žanr
+});
+
+// Funkcija za filtriranje predstava po izabranom žanru
+function filtrirajPoZanru() {
+    var zanrSelect = document.getElementById("zanr");
+    var izabraniZanr = zanrSelect.value.toLowerCase(); // Preuzimanje vrednosti izabranog žanra
+
+    // Filtriranje predstava prema izabranom žanru
+    var filtriranePredstave = predstave.filter(function(predstava) {
+        return izabraniZanr === "svi" || predstava.zanr.toLowerCase() === izabraniZanr;
+    });
+
+    // Prikazivanje filtriranih predstava
+    prikaziPredstave(filtriranePredstave);
+}
+
+
+// Pozivamo funkciju za popunjavanje padajućeg menija kada se stranica učita
+window.onload = function() {
+    popuniZanrove();
+    prikaziPredstave(predstave); // Prikažemo sve predstave kada se stranica učita
+};
+
+function prikaziDetalje(naziv) {
+    var odabranaPredstava = predstave.find(function(predstava) {
+        return predstava.naziv === naziv;
+    });
+    console.log("Otvoreno");
+    if (odabranaPredstava) {
+        var detaljiDiv = document.createElement("div");
+        detaljiDiv.classList.add("detalji-div");
+        detaljiDiv.innerHTML = `
+            <h3>${odabranaPredstava.naziv}</h3>
+            <p><strong>Жанр:</strong> ${odabranaPredstava.zanr}</p>
+            <p><strong>Режисер:</strong> ${odabranaPredstava.reziser}</p>
+            <p><strong>Трајање:</strong> ${odabranaPredstava.trajanje}</p>
+            <p><strong> Улоге: </strong> </p>
+            <p>${odabranaPredstava.uloge}</p>
+            <p><strong>Опис представе:</strong></p>
+            <p>${odabranaPredstava.opis}</p>
+            <button onclick="closePopup()">Затвори</button>
+        `;
+
+        // Check if the element exists before appending
+        var predstavaDiv = document.getElementById("popup"); // Change this line
+        if (predstavaDiv) {
+            predstavaDiv.appendChild(detaljiDiv);
+        } else {
+            console.error("Element with ID 'popup' not found.");
+        }
+    } else {
+        alert("Predstava nije pronađena!");
+    }
+}
+
+const popup = (naziv) => {
+    var popup = document.getElementById('popup'); // Get reference to the popup element
+    popup.innerHTML = "";
+    popup.style.display = 'block';
+    prikaziDetalje(naziv);
+}
+
+const closePopup = () => {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+
+
+// Funkcija za rezervaciju ulaznica za predstavu
+function rezervisiUlaznice(naziv) {
+    // Ovde implementiraj kod za rezervaciju ulaznica za odabranu predstavu
+}
+
+
+// Funkcija za zatvaranje modalnog prozora
+function zatvoriModal() {
+    var modalContainer = document.getElementById("modal-container");
+    modalContainer.style.display = "none";
+}
+
+
+// Funkcija za popunjavanje padajućeg menija sa žanrovima
+function popuniZanrove() {
+    var zanrSelect = document.getElementById("zanr");
+    var zanrovi = [];
+    // Iteriramo kroz sve predstave i dodajemo jedinstvene žanrove u niz
+    predstave.forEach(function(predstava) {
+        if (!zanrovi.includes(predstava.zanr)) {
+            zanrovi.push(predstava.zanr);
+        }
+    });
+    // Sortiramo niz žanrova po abecednom redu
+    zanrovi.sort();
+    // Popunjavamo padajući meni sa žanrovima
+    zanrovi.forEach(function(zanr) {
+        var option = document.createElement("option");
+        option.value = zanr.toLowerCase(); // Postavljamo vrednost malim slovima radi doslednosti
+        option.textContent = zanr;
+        zanrSelect.appendChild(option);
+        // Dodajemo event listener za promenu vrednosti svake opcije
+        option.addEventListener('change', function() {
+            filtrirajPoZanru();
+        });
+    });
+}
